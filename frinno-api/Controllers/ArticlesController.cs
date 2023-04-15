9ab590da598ed8112128b7b28e5a948a40f64971
Frank Leons <dev.frinno@gmail.com>
2023-04-15 20:22:27 +0100

    #FixedProfile#InArticles

--- frinno-api/Controllers/ArticlesController.cs
+++ frinno-api/Controllers/ArticlesController.cs
@@ -5,6 +5,7 @@ using frinno_core.Entities;
 using frinno_core.Entities.Articles;
 using frinno_core.Entities.Profiles;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace frinno_api.Controllers
@@ -16,31 +17,32 @@ namespace frinno_api.Controllers
     {
         private readonly IArticlesService<Article> articlesService;
         private readonly IProfileService<Profile> profileService;
-        public ArticlesController(IArticlesService<Article> articles,IProfileService<Profile> profiles)
+        private readonly UserManager<Profile> userManager;
+        public ArticlesController(IArticlesService<Article> articles,IProfileService<Profile> profiles,
+        UserManager<Profile> users)
         {
             articlesService = articles;
             profileService = profiles;
+            userManager = users;
         }
         //Creates a New Article Resource
-        [HttpPost("{profileId}")]
-        public ActionResult<CreateArticleResponse> CreateNew([FromBody] CreateArticleRequest request, string profileId)
+        [HttpPost("{profileId}"), Authorize(Roles ="Author")]
+        public async Task< ActionResult<CreateArticleResponse>> CreateNew([FromBody] CreateArticleRequest request, string profileId)
         {
-            var profileExists = profileService.ProfileExists(new Profile{ Id = profileId});
+            var profile = await userManager.FindByIdAsync(profileId);
 
-            if(!profileExists)
+            if(profile == null)
             {
-                return NotFound(new { Message = "Author not Found!." });
+                return NotFound(new { Message = "Profile Not found, Please Sign up!." });
             }
 
-            var author = profileService.FindById(profileId);
-
-
+        
             //Todo, Add Article Specific Validations
             var newArticle = new Article
             {
                 Title = request.Title,
                 LongText = request.LongText,
-                Author = author
+                Author = profile
             };
 
             var ArticleResponse = new Article();
@@ -55,11 +57,6 @@ namespace frinno_api.Controllers
                 return BadRequest(new { Message = ex.Message });
             }
 
-            if (newArticle == null)
-            {
-                return BadRequest("Failed to Create a Article!.");
-            }
-
             var response = new CreateArticleResponse
             {
                 Id = ArticleResponse.Id,
@@ -72,57 +69,49 @@ namespace frinno_api.Controllers
 
         //Updates a Article Resource
         [HttpPut("{Id}/{profileId}")]
-        public ActionResult<ArticleInfoResponse> UpdateArticle(int Id,string profileId, [FromBody] UpdateArticleRequest request)
+        public async Task<ActionResult<ArticleInfoResponse>> UpdateArticle(int Id,string profileId, [FromBody] UpdateArticleRequest request)
         {
-            var profileExists = profileService.ProfileExists(new Profile{ Id = profileId});
+            var profile = await userManager.FindByIdAsync(profileId);
 
-            if(!profileExists)
+            if(profile == null)
             {
-                return NotFound(new { Message = "Author not Found!." });
+                return NotFound(new { Message = "Author Not found, Please Sign up!." });
             }
+            var article = articlesService.FetchSingleById(Id);
 
-            var author = profileService.FindById(profileId);
-            var Article = articlesService.FetchSingleById(Id);
-
-            if (Article == null)
+            if (article == null)
             {
                 return NotFound($"Article: {Id} NotFound!.");
             }
 
-            if (request == null)
-            {
-                return BadRequest();
-            }
+            article.Title = request.Title;
+            article.LongText = request.LongText;
 
-            Article.Title = request.Title;
-            Article.LongText = request.LongText;
-
-            var ArticleResponse = articlesService.Update(Article);
+            var ArticleResponse = articlesService.Update(article);
 
             return Created(nameof(GetSingle), new { Message = $"Updated {ArticleResponse.Id}" });
         }
 
         //Removes Single Article Resource
         [HttpDelete("{Id}/{profileId}")]
-        public ActionResult<string> DeleteArticle(int Id, string profileId)
+        public async Task<ActionResult<bool>> DeleteArticle(int Id, string profileId)
         {
-            var profileExists = profileService.ProfileExists(new Profile{ Id = profileId});
+            var profile = await userManager.FindByIdAsync(profileId);
 
-            if(!profileExists)
+            if(profile == null)
             {
-                return NotFound(new { Message = "Author not Found!." });
+                return NotFound(new { Message = "Author Not found, Please Sign up!." });
             }
 
-            var author = profileService.FindById(profileId);
-            var data = articlesService.FetchSingleById(Id);
+            var article = articlesService.FetchSingleById(Id);
 
-            if (data == null)
+            if (article == null)
             {
                 return NotFound("Article Not found");
             }
 
-            articlesService.Remove(data);
-            return Ok("Article Delete Success.!");
+            articlesService.Remove(article);
+            return NoContent();
         }
 
         //Returns a Article Resource
@@ -176,53 +165,5 @@ namespace frinno_api.Controllers
            return Ok(response);
 
         }
-
-        // [HttpPost("{articleId}/{likedById}/likes")]
-        // public ActionResult<CreateArticleResponse> ArticleLikes(int articleId, string likedById, [FromBody] CreateArticleLikeRequest query)
-        // {
-        //     var profileExists = profileService.ProfileExists(new Profile{ Id = likedById});
-
-        //     if(!profileExists)
-        //     {
-        //         return NotFound(new { Message = "User not Found!." });
-        //     }
-
-        //     var user = profileService.FindById(likedById);
-
-        //     var author = profileService.FindById(likedById);
-        //     var data = articlesService.FetchSingleById(articleId);
-
-        //     if (data == null)
-        //     {
-        //         return NotFound("Article Not found");
-        //     }
-
-        //     data.Likes = new ArticleLike {
-        //         Likes = +1,
-        //         Profile = user,
-        //         Article = data
-        //     };
-
-            
-
-        //     var likesResponse = new Article();
-        //     try
-        //     {
-                
-        //         likesResponse = articlesService.Update(data);
-        //     }
-        //     catch (System.Exception)
-        //     {   
-        //         throw;
-        //     }
-
-        //     var response = new CreateArticleResponse {
-        //         Id  = likesResponse.Id ,
-        //         AuthorId = likesResponse.Author.Id,
-        //         Title = likesResponse.Title,
-        //         TotalLikes = likesResponse.Likes.Likes
-        //     };
-        //     return Created("", response);
-        // }
     }
 }
