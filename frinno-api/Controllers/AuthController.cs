2f50c4c0aa704900d302bbe3fc98b0edd1b633b6
Frank Leons <dev.frinno@gmail.com>
2023-11-04 13:26:50 +0300

    #MinorDevChanges

--- frinno-api/Controllers/AuthController.cs
+++ frinno-api/Controllers/AuthController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using frinno_application.Authentication;
 using frinno_core.DTOs;
 using frinno_core.Entities.Profile.ValueObjects;
